5796e4332897c4c13d36ab5ecc43ae5653e04e48
WebFreak001 <gh@webfreak.org>
2023-07-17 13:20:54 +0200

    initial commit

---
+++ nanahira_papercraft.gd
@@ -0,0 +1,27 @@
+extends Node3D
+
+#var animationPlayer: AnimationPlayer
+
+const VELOCITY_SCALE = 2.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#animationPlayer = get_node("AnimationPlayer")
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func set_velocity(velocity: Vector3):
+	var v = Vector3(velocity.x, 0, velocity.z).length() * VELOCITY_SCALE
+	if v > 0.01:
+		var speed = clamp(0.75, 2, v * 2)
+		var blend = clamp(0, 1, v * 2)
+		$AnimationTree["parameters/playback"].travel("Walk")
+		$AnimationTree.set("parameters/Walk/Blend2/blend_amount", blend);
+		$AnimationTree["parameters/Walk/TimeScale/scale"] = speed
+	else:
+		$AnimationTree["parameters/playback"].travel("Idle")
+		$AnimationTree["parameters/Walk/TimeScale/scale"] = 1.0
