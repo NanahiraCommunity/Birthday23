d525130332c4984882fac85b040a0106191b5e50
lightbulbmeow <62456216+lightbulbmeow@users.noreply.github.com>
2023-07-24 20:26:12 +0800

    Basic pause menu

---
+++ pause_menu.gd
@@ -0,0 +1,24 @@
+extends ColorRect
+
+@onready var animator: AnimationPlayer = $AnimationPlayer
+@onready var resume_button: Button = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/ResumeButton
+@onready var quit_button: Button = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/QuitButton
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_cancel"):
+		pause()
+
+func _ready():
+	resume_button.pressed.connect(unpause)
+	quit_button.pressed.connect(get_tree().quit)
+
+func unpause():
+	visible = false
+	get_tree().paused = false
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func pause():
+	visible = true
+	get_tree().paused = true
+	animator.play("Pause")
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
