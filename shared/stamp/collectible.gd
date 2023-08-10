4d2260788e2cc041dfcdd8bf19df06228123156a
WebFreak001 <gh@webfreak.org>
2023-08-10 23:35:59 +0200

    minor polish

--- shared/stamp/collectible.gd
+++ shared/stamp/collectible.gd
@@ -14,6 +14,7 @@ var test_index = 0
 
 func _ready():
 	test_index = randi_range(0, 2)
+	rotate_y(-sin(global_position.x) * cos(global_position.z) * TAU)
 
 func _process(delta):
 	rotate_y(delta * ROTATE_SPEED)
