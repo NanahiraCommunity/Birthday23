36cc860e534214792579ed84c46d0bb43efbb082
GammA <48187391+GammA-su@users.noreply.github.com>
2023-08-18 01:06:24 +0200

    Small edit to add instance.queue_free()

--- models/characters/character.gd
+++ models/characters/character.gd
@@ -97,6 +97,7 @@ func update_character():
 		self.add_child(instance)
 		var node = instance.get_node(respath)
 		node.reparent($Armature/Skeleton3D, false)
+		instance.queue_free()
 		if skin:
 			_reskin(node, skin)
 
