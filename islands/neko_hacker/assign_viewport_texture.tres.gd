e88aa1af4794aa0507e6115dd2bb9a161eb7fabd
WebFreak001 <gh@webfreak.org>
2023-08-20 04:43:09 +0200

    neko world phase 2

--- islands/neko_hacker/assign_viewport_texture.tres.gd
+++ islands/neko_hacker/assign_viewport_texture.tres.gd
@@ -2,9 +2,15 @@ extends Node3D
 
 var setup = false
 
+@export var from_override = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var this = self
-	var m = (this.mesh if this is MeshInstance3D else this).material.duplicate()
+	var m = null
+	if from_override:
+		m = this.get_surface_override_material(0)
+	else:
+		m = (this.mesh if this is MeshInstance3D else this).material.duplicate()
 	m.set_shader_parameter("viewport", get_viewport().get_texture())
 	this.material_override = m
