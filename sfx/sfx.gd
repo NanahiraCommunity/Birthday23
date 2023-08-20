cc4877ee1b6c0ffcd68cef8514e04c9e1e774375
WebFreak001 <gh@webfreak.org>
2023-08-20 16:04:51 +0200

    add SFX helper

---
+++ sfx/sfx.gd
@@ -0,0 +1,12 @@
+extends Node
+
+var index: int = 0
+@onready var streams: Array[Node] = get_children()
+
+func play(sound: AudioStream, from_position: float = 0.0):
+	var i = index
+	index += 1
+	if index >= streams.size():
+		index = 0
+	streams[i].stream = sound
+	streams[i].play(from_position)
