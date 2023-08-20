35abaa5aa7e7098252098d8903b1136ac74585f3
Nicocchi <nicocchilover@gmail.com>
2023-08-19 23:45:11 -0400

    merge conflict

--- pause_menu.gd
+++ pause_menu.gd
@@ -4,13 +4,6 @@ extends ColorRect
 @onready var resume_button: Button = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/ResumeButton
 @onready var quit_button: Button = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/QuitButton
 
-var _mainmenu_scene: String
-@export_file("*.tscn") var mainmenu_scene: String:
-	get:
-		return _mainmenu_scene
-	set(value):
-		_mainmenu_scene = value
-
 func _unhandled_input(event):
 	if event.is_action_pressed("ui_cancel"):
 		unpause() if is_visible() else pause()
@@ -18,11 +11,6 @@ func _unhandled_input(event):
 func _ready():
 	resume_button.pressed.connect(unpause)
 	quit_button.pressed.connect(get_tree().quit)
-
-# Option for title screen. Bug where going to title screen causes
-# no buttons to be pressed
-func mainmenu():
-	get_tree().change_scene_to_file(_mainmenu_scene)
 
 func unpause():
 	visible = false
